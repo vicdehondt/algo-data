18c0fb6230f4981f52d29da957d9f73f3565946f
Vic De Hondt <vicdehondt@Vics-MacBook-Pro.local>
2021-11-10 10:56:10 +0100

    oefening 7 WPO 1

--- WPO/WPO 1.rkt
+++ WPO/WPO 1.rkt
@@ -10,15 +10,6 @@
 ; (sum: (number -> number) (number -> number) number number -> number)
 ; (compose: (any -> any) (any -> any) -> (any -> any))
 
-
-; Oefening 8
-(define (all-but-first-n l n)
-  (define (iterate current counter)
-    (if (or (= counter 0) (null? current))
-        current
-        (iterate (cdr current) (- counter 1))))
-  (iterate l n))
-
 ; Oefening 6
 (define (last-of-list l)
   (define (iter lst result)
@@ -30,5 +21,40 @@
 (define (last-of-vector v)
   (let ((l (- (vector-length v) 1)))
     (if (zero? l)
-        '()
+        (display "Vector is empty!")
         (vector-ref v l))))
+
+; last-of-list: worst-case O(n)
+; last-of-vector: worst-case O(1)
+; last-of-list: best-case O(n)
+; last-of-vector: best-case O(1)
+; last-of-list: average-case O(n)
+; last-of-vector: average-case O(1)
+
+
+; Oefening 7
+(define (length-of-list l)
+  (define (iter lst result)
+    (if (null? lst)
+        result
+        (iter (cdr lst) (+ result 1))))
+  (iter l 0))
+
+(define (length-of-vector v)
+  (vector-length v))
+
+;length-of-list: worst-case O(n)
+;length-of-vector: worst-case O(1)
+;length-of-list: best-case O(n)
+;length-of-vector: best-case O(1)
+;length-of-list: average-case O(n)
+;length-of-vector: average-case O(1)
+
+
+; Oefening 8
+(define (all-but-first-n l n)
+  (define (iterate current counter)
+    (if (or (= counter 0) (null? current))
+        current
+        (iterate (cdr current) (- counter 1))))
+  (iterate l n))
