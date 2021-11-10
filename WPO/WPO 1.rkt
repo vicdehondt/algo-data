7c63fa95a5f24cff5339e4cd496cf6f88f19b75e
Vic De Hondt <vicdehondt@Vics-MacBook-Pro.local>
2021-11-10 09:35:40 +0100

    Oefening 1 WPO 1

---
+++ WPO/WPO 1.rkt
@@ -0,0 +1,11 @@
+; (cons: any any -> pair)
+; (car: pair -> any)
+; (cdr: pair -> any)
+; (vector-ref: vector number -> any)
+; (vector-set!: vector number any -> 0)
+; (member: any pair -> pair U {#f})
+
+; (map: (any -> any) pair -> pair)
+; (sum: (number -> number) (number -> number) number number -> number)
+; (compose: (any -> any) (any -> any) -> (any -> any))
+
