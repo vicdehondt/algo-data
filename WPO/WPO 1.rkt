6e008e05409e0923fa6b32563f5519224b4b2384
Vic De Hondt <vicdehondt@Vics-MacBook-Pro.local>
2021-11-10 09:42:17 +0100

    Oefening 1 WPO 1

--- WPO/WPO 1.rkt
+++ WPO/WPO 1.rkt
@@ -9,3 +9,11 @@
 ; (sum: (number -> number) (number -> number) number number -> number)
 ; (compose: (any -> any) (any -> any) -> (any -> any))
 
+
+; Oefening 8
+(define (all-but-first-n l n)
+  (define (iterate current counter)
+    (if (or (= counter 0) (null? current))
+        current
+        (iterate (cdr current) (- counter 1))))
+  (iterate l n))
