f4e9b291050adc4b20c4fc03de86721237bb875c
Vic De Hondt <vicdehondt@Vics-MacBook-Pro.local>
2021-11-26 17:58:03 +0100

    nieuwe WPO's

--- WPO/WPO 3.rkt
+++ WPO/WPO 3.rkt
@@ -39,24 +39,84 @@
 ; proper:  "o" "lo" "llo" "ello"
 
 ; Oef 6
-(define-library (brute-foce)
-  (export match)
-  (import (scheme base))
+(define (match-1 t p)
+  (define n-t (string-length t))
+  (define n-p (string-length p))
+  (let loop
+    ((i-t 0)
+     (i-p 0)
+     (res '()))
+    (cond
+      ((> i-p (- n-p 1))
+       (loop (+ i-t 1) 0 (cons i-t res)))
+      ((> i-t (- n-t n-p))
+       (reverse res))
+      ((eq? (string-ref t (+ i-t i-p)) (string-ref p i-p))
+       (loop i-t (+ i-p 1) res))
+      (else
+       (loop (+ i-t 1) 0 res)))))
 
-  (begin
+; Oef 7
+(define (match-2 t p w) ; vb (match "hallo hello" "h.llo" #\.)
+  (define n-t (string-length t))
+  (define n-p (string-length p))
+  (let loop
+    ((i-t 0)
+     (i-p 0)
+     (res '()))
+    (cond
+      ((> i-p (- n-p 1))
+       (loop (+ i-t 1) 0 (cons i-t res)))
+      ((> i-t (- n-t n-p))
+       (reverse res))
+      ((or (eq? (string-ref t (+ i-t i-p)) (string-ref p i-p)) (eq? (string-ref p i-p) w))
+       (loop i-t (+ i-p 1) res))
+      (else
+       (loop (+ i-t 1) 0 res)))))
+
+; Oef 11
+(define (compute-failure-function p)
+      (define n-p (string-length p))
+      (define sigma-table (make-vector n-p 0))
+      (let loop
+        ((i-p 2)
+         (k 0))
+        (when (< i-p n-p)
+          (cond
+            ((eq? (string-ref p k) 
+                  (string-ref p (- i-p 1)))
+             (vector-set! sigma-table i-p (+ k 1))
+             (loop (+ i-p 1) (+ k 1)))
+            ((> k 0)
+             (loop i-p (vector-ref sigma-table k)))
+            (else ; k=0
+             (vector-set! sigma-table i-p 0)
+             (loop (+ i-p 1) k))))
+        (vector-set! sigma-table 0 -1)
+        (lambda (q)
+          (vector-ref sigma-table q))))
+ 
     (define (match t p)
       (define n-t (string-length t))
       (define n-p (string-length p))
+      (define sigma (compute-failure-function p))
+      (display t)
+      (newline)
       (let loop
         ((i-t 0)
-         (i-p 0)
-         (res '()))
-        (cond
+         (i-p 0))
+        (cond 
           ((> i-p (- n-p 1))
-           (loop (+ i-t 1) 0 (cons i-t res)))
+           (newline)
+           (display "Gedaan!"))
           ((> i-t (- n-t n-p))
-           (reverse res))
+           #f)
           ((eq? (string-ref t (+ i-t i-p)) (string-ref p i-p))
-           (loop i-t (+ i-p 1) res))
+           (loop i-t (+ i-p 1)))
           (else
-           (loop (+ i-t 1) 0 res)))))))
+           (loop (+ i-t (- i-p (sigma i-p))) (if (> i-p 0)
+                                                 (sigma i-p)
+                                                 0))
+           (display #\space)))))
+
+; Oef 12
